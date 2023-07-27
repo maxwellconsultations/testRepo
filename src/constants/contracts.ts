5a45f4aa43efbc743cb72bc1dec241342fd244d1
ibeandyy <adhyr4@gmail.com>
2023-07-26 20:05:51 -0700

    init commit, scoping

---
+++ src/constants/contracts.ts
@@ -0,0 +1,2 @@
+export const LBPPROXY = "0X";
+export const TESTLBP = "0x4131fD7B699155f69E0192145C36f27852BE7c11";
---
+++ src/index.ts
@@ -0,0 +1,48 @@
+import { Contract, InfuraProvider, InfuraWebSocketProvider } from "ethers";
+import "dotenv/config";
+import { TESTLBP } from "./constants/contracts";
+import { LBPABI } from "./constants/lbpabi";
+
+if (!process.env.INFURA_MAINNET) {
+  throw new Error("Missing INFURA_MAINNET environment variable");
+}
+const ethProvider = new InfuraWebSocketProvider(
+  "homestead",
+  process.env.INFURA_MAINNET
+);
+
+const ethProvider2 = new InfuraProvider(
+  "homestead",
+  process.env.INFURA_MAINNET
+);
+
+const testLBP = new Contract(TESTLBP, LBPABI, ethProvider);
+const testLBP2 = new Contract(TESTLBP, LBPABI, ethProvider2);
+
+//TO GET POOL TOKENS:
+//balancerVault.getPoolTokens(LBP ID)
+
+/*TO GET POOL STARTING TOKENS: 
+LISTEN FOR PoolBalanceChanged EVENT ON POOL VAULT CONTRACT
+PoolBalanceChanged (index_topic_1 bytes32 poolId, index_topic_2 address liquidityProvider, address[] tokens, int256[] deltas, uint256[] protocolFeeAmounts)
+DELTAS ARE THE INTIAL TOKEN VALUES
+*/
+
+/*TO GET POOL INTIAILIZATOIN
+LISTEN FOR POOLCREATED ON FjordLbpProxyV6
+event PoolCreated(
+        address indexed pool,
+        bytes32 poolId,
+        string name,
+        string symbol,
+        address[] tokens,
+        uint256[] weights,
+        uint256 swapFeePercentage,
+        address owner,
+        bool swapEnabledOnStart,
+        LBPType lbpType
+    );
+*/
+//ba=balanceA, bb=balanceB, wa=weightA, wb=weightB
+//price_A = (ba ^ wa) / (bb ^ wb)
+//price_B = (bb ^ wb) / (ba ^ wa)
